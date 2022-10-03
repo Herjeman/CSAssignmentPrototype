52cc11928e86b27b4307acaef8cf482643f91ce3
Rasmus8844 <rasmus.mollerstedt@edu.futuregames.nu>
2022-10-03 17:23:12 +0200

    continue screen basic template

---
+++ Assets/Scripts/StartMenuOnlyScripts/ContinueScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ContinueScreen : MonoBehaviour
+{
+
+
+
+
+
+
+
+
+    public void Continue()
+    {
+
+        SceneManager.LoadScene("Level");
+
+
+    }
+
+
+}
